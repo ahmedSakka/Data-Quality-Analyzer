04af018f95aeeff2b896225ee5b7e76746f4a41f
Ahmed-Sakka <103361512+ahmedSakka@users.noreply.github.com>
2025-05-24 13:33:56 +0300

    Update main function to include reporting

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import typer
 import pandas as pd
 import json
 from commands.quality_check import quality_check
+from commands.reporting import save_json, save_markdown
 
 app = typer.Typer()
 
 @app.command()
-def analyze(file_path: str, save: bool = False):
+def analyze(file_path: str, 
+            save: bool = typer.Option(False, "--save" ,help="Save the report."),
+            format: str = typer.Option("json", help="Format of the report (json or markdown)")):
     try:
         df = pd.read_csv(file_path)
     except Exception as e:
@@ -17,13 +20,20 @@ def analyze(file_path: str, save: bool = False):
     typer.echo("Performing quality check...")
 
     report = quality_check(df)
+
     typer.echo("Quality check completed.")
-    typer.echo(json.dumps(report, indent=4))
 
     if save:
-        with open("Report.json", "w") as f:
-            json.dump(report, f, indent=4)
-            typer.echo("\nReport saved to 'Report.json'")
+        if format == "json":
+            path = save_json(report)
+        elif format == "markdown":
+            path = save_markdown(report)
+        else:
+            typer.echo("Invalid format. Please choose 'json' or 'markdown'")
+            raise typer.Exit(code=1)
+        typer.echo(f"Report saved to: {path}")
+    else:
+        typer.echo(json.dumps(report, indent=4))
 
 if __name__ == "__main__":
     app()
