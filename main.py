7cd9f11bced30c2458847e97e8b3cfe658de3a4d
Ahmed-Sakka <103361512+ahmedSakka@users.noreply.github.com>
2025-05-24 15:23:22 +0300

    Add: checking specific columns and displaying a concise summary of the report

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import pandas as pd
 import json
 from commands.quality_check import quality_check
 from commands.reporting import save_json, save_markdown
+from typing import List, Optional
 
 app = typer.Typer()
 
 @app.command()
 def analyze(file_path: str, 
             save: bool = typer.Option(False, "--save" ,help="Save the report."),
-            format: str = typer.Option("json", help="Format of the report (json or markdown)")):
+            format: str = typer.Option("json", help="Format of the report (json or markdown). Default is json."),
+            summary: bool = typer.Option(False, "--summary", help= "Show a concise summary of the report."),
+            columns: Optional[List[str]] = typer.Option(None, "--columns", help="Specify columns to analyze. If not provided, all columns will be analyzed.")):
     try:
         df = pd.read_csv(file_path)
     except Exception as e:
@@ -17,13 +20,32 @@ def analyze(file_path: str,
         raise typer.Exit(code=1)
     
     typer.echo("File loaded successfully.")
-    typer.echo("Performing quality check...")
+    
+    # Running quality checks on specefied columns or all columns
+    if columns:
+        missing_cols = [col for col in columns if col not in df.columns]
+        if missing_cols:
+            typer.echo(f"These columns do not exist in the DataFrame: {', '.join(missing_cols)}")
+            raise typer.Exit(code=1)
+        df = df[columns]
+        typer.echo(f"Analyzing specified columns: {', '.join(columns)}")
+    else:
+        typer.echo("Analyzing all columns in the DataFrame.")
 
     report = quality_check(df)
 
     typer.echo("Quality check completed.")
 
-    if save:
+    # Displaying summary or saving the report
+    if summary:
+        typer.echo("Summary Report:")
+        typer.echo(json.dumps({
+            "missing_values": report["missing_values"],
+            "duplicate_rows": report["duplicate_rows"],
+            "constant_columns": report["constant_columns"]
+        }, indent=4))
+
+    elif save:
         if format == "json":
             path = save_json(report)
         elif format == "markdown":
